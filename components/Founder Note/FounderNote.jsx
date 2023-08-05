595669513030047ad696737c78b8dcaab25efb4d
Dibyendu Saha <dip4012@gmail.com>
2023-08-05 20:39:44 +0530

    Centered the founder note section title

--- components/Founder Note/FounderNote.jsx
+++ components/Founder Note/FounderNote.jsx
@@ -5,7 +5,7 @@ const FounderNote = () => {
 	return (
 		<section className="w-full px-[80px] py-[50px] bg-white relative z-0 max-[767px]:px-[20px]">
 			{/* section header */}
-			<h1 className="text-[#350B63] font-Raleway text-5xl font-bold leading-[62.4px] tracking-[-2.88px] text-center max-[767px]:text-left max-[767px]:text-2xl max-[767px]:leading-[31.2px] max-[767px]:tracking-[-1.44px]">
+			<h1 className="text-[#350B63] font-Raleway text-5xl font-bold leading-[62.4px] tracking-[-2.88px] text-center max-[767px]:text-2xl max-[767px]:leading-[31.2px] max-[767px]:tracking-[-1.44px]">
 				<span className="section_graphic">Note From The Founder</span>
 			</h1>
 
